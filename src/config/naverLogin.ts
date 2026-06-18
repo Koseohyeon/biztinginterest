5ae757f90508c0b1842df56bea88b01fdd5951bc
Koseohyeon <ksh030818@naver.com>
2026-06-18 14:23:29 +0900

    N-Pass 초록화면 추가

--- src/config/naverLogin.ts
+++ src/config/naverLogin.ts
@@ -1,5 +1,6 @@
 export const NAVER_LOGIN = {
-  freshmart: { code: "DQrTZu", pageId: "freshmart" },
+  freshmart1: { code: "DQrTZu", pageId: "freshmart1" },
+  freshmart2: { code: "6qfrvN", pageId: "freshmart2" },
   // 새 페이지 생길 때마다 여기에만 추가
   // eventPage:  { code: "ABC123", pageId: "event-page" },
   // summerSale: { code: "XYZ789", pageId: "summer-sale" },
