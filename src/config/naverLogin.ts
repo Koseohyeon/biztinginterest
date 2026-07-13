9be203afd2d4ad8cb37b8f73b76eb14f7e8edc5e
Koseohyeon <ksh030818@naver.com>
2026-07-13 10:30:10 +0900

    대고객용 N-Pass 예시 URL

--- src/config/naverLogin.ts
+++ src/config/naverLogin.ts
@@ -1,7 +1,7 @@
 export const NAVER_LOGIN = {
   freshmart1: { code: "DQrTZu", pageId: "freshmart1" },
   freshmart2: { code: "6qfrvN", pageId: "freshmart2" },
-  Personalbizting: { code: "6qfrvN", pageId: "Personalbizting" },
+  Personalbizting: { code: "4gj4SE", pageId: "Personalbizting" },
 
   // 새 페이지 생길 때마다 여기에만 추가
   // eventPage:  { code: "ABC123", pageId: "event-page" },
