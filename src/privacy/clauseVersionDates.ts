f72e03d53473dde869e45165057973aa0111b20e
Koseohyeon <ksh030818@naver.com>
2026-09-21 16:55:39 +0900

    시행일 수정

--- src/privacy/clauseVersionDates.ts
+++ src/privacy/clauseVersionDates.ts
@@ -31,4 +31,4 @@ export const PRIVACY_V13_EFFECTIVE_AT = new Date("2026-09-26T00:00:00+09:00");
 export const TERMS_V12_NOTICE_START_AT = new Date("2026-09-28T00:00:00+09:00");
 
 // Bizting 이용약관 개정본이 예정 버전에서 현재 버전으로 전환되는 시행 시점입니다. (시행일자)
-export const TERMS_V12_EFFECTIVE_AT = new Date("2026-10-18T00:00:00+09:00");
+export const TERMS_V12_EFFECTIVE_AT = new Date("2026-10-28T00:00:00+09:00");
